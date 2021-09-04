8bad6ed229b5ad1d72e88f2d918560fca0cea2bb
Snehalmiraje <47252937+Snehalmiraje@users.noreply.github.com>
2021-09-04 16:21:01 +0530

    Add files via upload

---
+++ front end/customer/src/component/Title.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Title extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+                    <div class="container1">
+                        <div class="title">
+                        <h1 >Car Wash</h1></div>
+                    </div>
+                <div class="desc">
+                </div>
+                <div class="inf">     <p>Enjoy the spirit of a new car by Adding Shine in every side</p> </div>
+                </div>
+        
+        );
+    }
+
+}
